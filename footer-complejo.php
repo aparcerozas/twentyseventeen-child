40164119f64643096eccc63b5b787d1e7964572f
EC2 Default User <ec2-user@ip-172-31-40-17.eu-central-1.compute.internal>
2019-12-13 10:28:24 +0000

    Inicio

---
+++ footer-complejo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="wrap">
+				<!DOCTYPE html>
+					<html>
+						<h2>Contactos</h2>
+						<ul>
+							<li>Teléfono: 696969696</li>
+							<li>Correo electrónico: holamundo@html.org</li>
+							<li>Fáx: ...Pero quién usa fáx en este siglo</li>
+						</ul>
+						<p>Si está leyendo esto, eso significa que ha aceptado nuestros términos de uso 
+						y tenemos derecho a robarle todo su dinero</p>
+					</html>
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer('complejo'); ?>
+
+</body>
+</html>
---
+++ footer-simple.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="wrap">
+				<!DOCTYPE html>
+					<html>
+						<h2>Contactos</h2>
+						<ul>
+							<li>Teléfono: 696969696</li>
+							<li>Correo electrónico: holamundo@html.org</li>
+							<li>Fáx: ...Pero quién usa fáx en este siglo</li>
+						</ul>
+					</html>
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer('simple'); ?>
+
+</body>
+</html>
